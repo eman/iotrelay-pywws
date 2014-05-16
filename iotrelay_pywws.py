d5fbd09dd4dc890afdd295aa618044a93917dd5a
Emmanuel Levijarvi <emansl@gmail.com>
2014-05-15 22:27:37 -0700

    use last reading timestamp as the early endpoint when fetching new readings

--- iotrelay_pywws.py
+++ iotrelay_pywws.py
@@ -21,20 +21,14 @@ class Poll(object):
     def __init__(self, config):
         self.keys = [k.strip() for k in config['series keys'].split(',')]
         self.data_store = config['data store']
-        poll_frequency = int(config['poll frequency'])
-        self.delta = datetime.timedelta(seconds=poll_frequency)
         lookback = int(config.get('lookback', DEFAULT_LOOKBACK))
         hours = datetime.timedelta(hours=lookback)
         self.last_ts = datetime.datetime.utcnow() - hours
 
     def get_readings(self):
-        current_timestamp = datetime.datetime.utcnow()
-        if current_timestamp < self.last_ts + self.delta:
-            logger.debug('pywws {0!s} until next reading'.format(
-                         self.last_ts + self.delta - current_timestamp))
-            return ()
-        readings = DataStore.data_store(self.data_store)
-        for reading in readings[self.last_ts:]:
+        for data in DataStore.data_store(self.data_store)[self.last_ts:]:
+            if data['idx'] <= self.last_ts:
+                continue
+            self.last_ts = data['idx']
             for key in self.keys:
-                yield Reading(READING_TYPE, reading[key], reading['idx'], key)
-        self.last_ts = current_timestamp
+                yield Reading(READING_TYPE, data[key], data['idx'], key)
